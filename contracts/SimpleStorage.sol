6d56eef0b943d4293fbafae2f674e55ea5c13046
raopratham19 <134732286+raopratham19@users.noreply.github.com>
2023-05-26 23:45:17 +0530

    Add files via upload

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,39 @@
+pragma solidity >=0.4.24;
+
+contract Ballot {
+   struct Documents {
+     string aadhar;
+}
+
+struct MultiSig
+{ 
+    address inst;
+    address stud;
+    Documents documents;    
+}
+mapping(address=>MultiSig) public wallets ;
+
+
+function createNewMultiSigbyUser(address instituteaddress) public {
+        
+        MultiSig storage wa = wallets[msg.sender];
+        wa.inst=instituteaddress;
+        wa.stud=msg.sender;
+}
+
+function uploadAadhar(string memory a) public{    
+    MultiSig storage wa=wallets[msg.sender];    
+}
+}
+
+
+
+function uploadAadhar(string a){
+    
+    var wa=wallets[msg.sender];
+
+    
+    
+    
+}
+
